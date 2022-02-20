e1fc55bb74cd1a9af26137d694dbe5dd6b48901d
Olena Chobanu <elenachobanu@gmail.com>
2022-02-20 09:58:32 +0200

    first commit

---
+++ gulp/tasks/scss.js
@@ -0,0 +1,63 @@
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import rename from 'gulp-rename';
+
+import cleanCss from 'gulp-clean-css';
+import webpcss from 'gulp-webpcss';
+import autoprefixer from 'gulp-autoprefixer';
+// import groupCssMediaQueries from 'group-css-media-queries'; - Не работает!!
+
+const sass = gulpSass(dartSass);
+
+export const scss = () => {
+    return app.gulp.src(app.path.src.scss, { sourcemaps: app.isDev })
+        .pipe(app.plugins.plumber(
+            app.plugins.notify.onError({
+                title: "SCSS",
+                message:"Error: <%= error.message %>"
+            })))
+        .pipe(app.plugins.replace(/@img\//g, '../img/'))
+        .pipe(sass({
+            outputStyle: 'expanded'
+        }))
+        // .pipe( - Не работает!!
+        //     app.plugins.if( - Не работает!!
+        //         app.isBuild, - Не работает!!
+        //         groupCssMediaQueries() - Не работает!!
+        //     )
+        // )
+        .pipe(
+            app.plugins.if(
+                app.isBuild,
+                autoprefixer({
+                    grid: true,
+                    overrideBrowserlist: ["last 3 versions"],
+                    cascade: true
+                })
+            )
+        )
+        .pipe(
+            app.plugins.if(
+                app.isBuild,
+                webpcss(
+                    {
+                        webpClass: ".webp",
+                        noWebpClass: ".no-webp"
+                    }
+                )
+            )
+        )
+        // Копирует несжатую версию файла Css
+        .pipe(app.gulp.dest(app.path.build.css))
+        .pipe(
+            app.plugins.if(
+                app.isBuild,
+                cleanCss()
+            )
+        )
+        .pipe(rename({
+            extname: ".min.css"
+        }))
+        .pipe(app.gulp.dest(app.path.build.css))
+        .pipe(app.plugins.browsersync.stream());
+}
